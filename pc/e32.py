428d3b79cc59eba43aee1aa519515c855b0328ef
chenxinming <chen_xin_ming@163.com>
2013-08-01 13:15:19 -0400

    Initial git ver, 0.9.1

---
+++ pc/e32.py
@@ -0,0 +1,19 @@
+
+import pygame
+
+try:
+    import android
+except ImportError:
+    android = None
+
+def ao_sleep(period):
+    pygame.time.wait(int(period*1000))
+
+def ao_yield():
+    # Android-specific:
+    if android:
+        if android.check_pause():
+            android.wait_for_resume()
+
+def reset_inactivity():
+    pass
